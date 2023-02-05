b19002d2fb4ebee0e9469d37e74b3860724bb90a
usihanselm <nots29vv@gmail.com>
2023-02-05 13:00:29 +0100

    replacing normal hardhat with FVM-Hardhat-Kit

---
+++ Hardhat/Bounty_Dutch_Auction/contracts/Svault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+
+// create a deal, monitor a deal, renew a deal, replicate data, insure data
+contract Svault {
+
+
+    // 1 create a deal with an offchain replication worker
+
+
+
+
+}
